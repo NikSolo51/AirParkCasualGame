77184d4136773f1fc3a3262e1deb6243cfa9a171
NikSolo51 <50959880+NikSolo51@users.noreply.github.com>
2020-10-26 15:12:36 +0300

    Add Debug Script and corrected createpointscript

--- Assets/[0]Source/Scripts/ControlPeople/Core/ControlPeople.cs
+++ Assets/[0]Source/Scripts/ControlPeople/Core/ControlPeople.cs
@@ -11,48 +11,25 @@ using UnityEngine.UI;
 [BurstCompile]
 public class ControlPeople : MonoBehaviour
 {
+    public static ControlPeople Instance;
     public static readonly ControlPeopleBehaviour ControlPeopleBehaviour = new ControlPeopleBehaviour();
     public static readonly MovePeopleToPointInDictionary MovePeopleToPointInDictionary = new MovePeopleToPointInDictionary();
-    
-    public List<Transform> debugList;
-    public List<Vector3> ValueList = new List<Vector3>();
-    
-    public int Queue;
-
+    public Clearing Clearing = new Clearing();
     public float TimeToClear = 1;
-    public Text ClearTime;
     
-    public static ControlPeople Instance;
-
-
-    private void Start()
-    {
-        debugList = ControlPeopleBehaviour.LinkList;
-        ValueList = ControlPeopleBehaviour.ListOfDictionariesOfPointsAndPeople[Queue].Values.ToList();
-    }
-
     private void Update()
     {
-        float.TryParse(ClearTime.text, out TimeToClear);
-
-        for (int i = 0; i < ControlPeople.ControlPeopleBehaviour.peopleDictionary.Count; i++)
-        {
-            ControlPeopleBehaviour.localPeoplePosition[i] =
-               ControlPeopleBehaviour.peopleDictionary.ElementAt(i).Value.localPosition;
-        }
-
-        Queue = ControlPeopleBehaviour.queue;
-
+        
         MovePeopleToPointInDictionary.MoveToPoint();
 
         if (Input.GetMouseButtonUp(0))
         {
             if (CreatePointsAtMousePosition.Instance.CanICreatePointInThisPlace())
             {
-                Clearing.Instance.AddToClear(ControlPeopleBehaviour.ListOfDictionariesOfPointsAndPeople[Queue], TimeToClear);
+                Clearing.Instance.AddToClear(ControlPeopleBehaviour.ListOfDictionariesOfPointsAndPeople[ControlPeopleBehaviour.queue], TimeToClear);
                 
                 MovePeopleToPointInDictionary.AddPointForMoveToPoint(
-                    ControlPeople.ControlPeopleBehaviour.ListOfDictionariesOfPointsAndPeople[Queue]);
+                    ControlPeople.ControlPeopleBehaviour.ListOfDictionariesOfPointsAndPeople[ControlPeopleBehaviour.queue]);
                 
                 ControlPeople.ControlPeopleBehaviour.IncrementQueue();
                 ControlPeople.ControlPeopleBehaviour.ClearIfQueueEqualsZero();
--- Assets/[0]Source/Scripts/ControlPeople/DrawSystem/CreatePointsAtMousePosition.cs
+++ Assets/[0]Source/Scripts/ControlPeople/DrawSystem/CreatePointsAtMousePosition.cs
@@ -18,13 +18,12 @@ public class CreatePointsAtMousePosition : MonoBehaviour
 
     [Header("[Minimum distance between two points]")] [SerializeField]
     private float minOffset = 0.5f;
+
     public float different = 1f;
     
-    public Text textForDifferentFloat;
-
     private readonly List<Vector3> listOfAllPoints = new List<Vector3>();
-    public  List<Vector3> coordinatesList = new List<Vector3>();
-    
+    public List<Vector3> coordinatesList = new List<Vector3>();
+
     public static CreatePointsAtMousePosition Instance;
 
     [SerializeField] private GameObject debugPrefab;
@@ -33,7 +32,7 @@ public class CreatePointsAtMousePosition : MonoBehaviour
     public Vector3 localValueCoord;
 
     public Vector3 coord;
-    
+
     private void Start()
     {
         Instance = this;
@@ -41,22 +40,21 @@ public class CreatePointsAtMousePosition : MonoBehaviour
 
     public void Update()
     {
-        float.TryParse(textForDifferentFloat.text, out different);
+        
 
         if (Input.GetMouseButton(0))
         {
             Generate();
         }
     }
-    
-    
+
+
     private void Generate()
     {
         //Get Coordinates  mouse position in local space
-        localValueCoord = CalculateLocalPointOnScreen(OriginSystem,2);
-      
-       
-        
+        localValueCoord = CalculateLocalPointOnScreen(OriginSystem, 2);
+
+
         //Check does it contain an already existing point pointsList
         //and checks if there is a minimum distance between two points 
         if (listOfAllPoints.Count > 0)
@@ -70,10 +68,10 @@ public class CreatePointsAtMousePosition : MonoBehaviour
             !IsThereADifferenceBetweenTheTwoPoints(ControlPeople.ControlPeopleBehaviour.localPeoplePosition,
                 localValueCoord, different))
             return;
-        
-        
+
+
         listOfAllPoints.Add(localValueCoord);
-        
+
 
         if (coordinatesList.Count < ControlPeople.ControlPeopleBehaviour.GetPeopleDictionaryCount())
         {
@@ -84,35 +82,46 @@ public class CreatePointsAtMousePosition : MonoBehaviour
         {
             ControlPeople.ControlPeopleBehaviour.ChooseTheNearestPerson();
         }
-        
+
         if (debugObjects.Count >= coordinatesList.Count)
             return;
         GameObject point = PointVisualization(debugPrefab, OriginSystem, localValueCoord, Vector3.one / 2);
         debugObjects.Add(point);
-        
     }
 
-    
-    public bool CanICreatePointInThisPlace()
+    public Vector3 CalculateLocalPointOnScreen(Transform Origin, float beamLength)
     {
-        if (coordinatesList.Count != 0)
-        {
-            if (listOfAllPoints.Count > 0)
-            {
-                if (IsThereADifferenceBetweenTheTwoPoints(coordinatesList.GetRange(0, coordinatesList.Count - 1),
-                    coord, different))
-                {
-                    return true;
-                }
-            }
-               
-        }
-        return false;
-        
+        Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 coordinates = mRay.origin + (mRay.direction * 12f);
+        coordinates = Origin.transform.InverseTransformPoint(coordinates);
+        coordinates.z = beamLength;
+        return coordinates;
     }
-   
-    
-    public bool IsThereADifferenceBetweenTheTwoPoints(List<Vector3> pointsList, Vector2 point , float allowedDifference)
+
+    public bool IsTheCoordinateInTheList(List<Vector3> listCoordinates, Vector3 coordinate)
+    {
+        //if the coordinate is in the list of all points, then we skip it
+        if (listCoordinates.Contains(coordinate))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private bool IsThereADifferenceInDistanceBetweenTwoPoints(List<Vector3> listCoordinates, Vector3 coordinate)
+    {
+        //check if there is a minimum distance between two points 
+        if (Vector3.Distance(coordinate, listCoordinates[listCoordinates.Count - 1]) <= minOffset)
+        {
+            return true; //skip the point
+        }
+
+        return false;
+    }
+
+
+    public bool IsThereADifferenceBetweenTheTwoPoints(List<Vector3> pointsList, Vector2 point, float allowedDifference)
     {
         float distanceToClosestPoint = Mathf.Infinity;
         Vector3 closestPoint = Vector3.zero;
@@ -121,7 +130,6 @@ public class CreatePointsAtMousePosition : MonoBehaviour
         foreach (Vector3 currentPoint in pointsList)
         {
             {
-               
                 float distanceToPoint = (point - new Vector2(currentPoint.x, currentPoint.y))
                     .sqrMagnitude;
                 if (distanceToPoint < distanceToClosestPoint)
@@ -150,9 +158,7 @@ public class CreatePointsAtMousePosition : MonoBehaviour
     }
 
 
-    
-    
-    private GameObject PointVisualization(GameObject Prefab,Transform Origin,Vector3 coordinate , Vector3 scale)
+    private GameObject PointVisualization(GameObject Prefab, Transform Origin, Vector3 coordinate, Vector3 scale)
     {
         GameObject point = Instantiate(debugPrefab, Origin);
         point.transform.localPosition = coordinate;
@@ -160,39 +166,20 @@ public class CreatePointsAtMousePosition : MonoBehaviour
         return point;
     }
 
-    public Vector3 CalculateLocalPointOnScreen(Transform Origin, float beamLength)
+    public bool CanICreatePointInThisPlace()
     {
-        Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Vector3 coordinates = mRay.origin + (mRay.direction * 12f);
-        coordinates = Origin.transform.InverseTransformPoint(coordinates);
-        coordinates.z = beamLength;
-        return coordinates;
-    }
-
-    private bool IsThereADifferenceInDistanceBetweenTwoPoints(List<Vector3> listCoordinates,Vector3 coordinate)
-    {
-        
-        //check if there is a minimum distance between two points 
-        if (Vector3.Distance(coordinate, listCoordinates[listCoordinates.Count - 1]) <= minOffset)
+        if (coordinatesList.Count != 0)
         {
-            return true; //skip the point
+            if (listOfAllPoints.Count > 0)
+            {
+                if (IsThereADifferenceBetweenTheTwoPoints(coordinatesList.GetRange(0, coordinatesList.Count - 1),
+                    coord, different))
+                {
+                    return true;
+                }
+            }
         }
 
         return false;
     }
-
-    public bool IsTheCoordinateInTheList(List<Vector3> listCoordinates,Vector3 coordinate)
-    {
-        //if the coordinate is in the list of all points, then we skip it
-        if (listCoordinates.Contains(coordinate))
-        {
-            return true;
-        }
-
-        return false;
-
-    }
-        
-
-    
 }
